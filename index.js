cccef24a18334a29c3adfcee6a5c6bdc24afb33a
shaunvan119 <shaunvan@live.com.au>
2022-04-21 10:16:57 +1000

    fecth api

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+
+
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-  .then(response => response.json())
-  .then(data => {
-      const postArr = data.slice(0, 5)
-      console.log(postArr)
-  })
-    
+    .then(res => res.json())
+    .then(data => {
+        const postsArr = data.slice(0, 5)
+        let html = ""
+        for (let post of postsArr) {
+            html += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `
+        }
+        document.getElementById("blog-list").innerHTML = html
+    })
